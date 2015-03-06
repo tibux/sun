47158d49d4665e4e88008f1a8513a695cf0c575f
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2015-03-06 23:03:53 +0200

    update metadata

---
+++ src/__metadata__.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# __metadata__.py is a path of sun.
+
+# Copyright 2015 Dimitris Zlatanidis <d.zlatanidis@gmail.com>
+# All rights reserved.
+
+# sun is a tray notification applet for informing about
+# package updates in Slackware.
+
+# https://github.com/dslackw/sun
+
+# sun is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+
+__all__ = "sun"
+__author__ = "dslackw"
+__version_info__ = (1, 0, 0)
+__version__ = "{0}.{1}.{2}".format(*__version_info__)
+__license__ = "GNU General Public License v3 (GPLv3)"
+__email__ = "d.zlatanidis@gmail.com"
+
+
+updater = "slackpkg"
+changelog_txt = "ChangeLog.txt"
+icon_path = "/usr/share/pixmaps/"
+conf_path = "/etc/{0}/".format(__all__)
+etc_slackpkg = "/etc/{0}/".format(updater)
+var_lib_slackpkg = "/var/lib/{0}/".format(updater)
+arch = x = "64" if os.uname()[4] == "x86_64" else ""
