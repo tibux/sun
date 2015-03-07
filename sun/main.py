175f87384c6c10cc8308c444a2345df0416f0746
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2015-03-07 15:58:27 +0200

    add status running

--- sun/main.py
+++ sun/main.py
@@ -22,11 +22,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-
 import time
 import pynotify
-
-
 from utils import (
     config,
     file_size,
