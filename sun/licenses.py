987a6cf3ea1c1c747da23064ae965740af707286
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2015-03-17 18:41:56 +0200

    added licenses

---
+++ sun/licenses.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# license.py is a path of sun.
+
+# Copyright 2015 Dimitris Zlatanidis <d.zlatanidis@gmail.com>
+# All rights reserved.
+
+# sun is a tray notification applet for informing about
+# package updates in Slackware.
+
+# https://github.com/dslackw/sun
+
+# sun is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from __metadata__ import (
+    __copyright__,
+    __email__
+)
+
+
+abt = ("SUN (Slackware Update Notifier)\n\n"
+       "Copyright {0} © Dimitris Zlatanidis\n"
+       "Email: {1}\n"
+       "Slackware® is a Registered Trademark of "
+       "Slackware Linux, Inc.\n"
+       "Linux is a Registered Trademark of Linus "
+       "Torvalds.".format(__copyright__, __email__))
+
+
+lic = ["Sun is a tray notification applet for informing about package",
+       "updates in Slackware.",
+       "Copyright (C) 2015  dslackw\n",
+       "This program is free software: you can redistribute it and/or modify",
+       "it under the terms of the GNU General Public License as published by",
+       "the Free Software Foundation, either version 3 of the License, or",
+       "(at your option) any later version.\n",
+       "This program is distributed in the hope that it will be useful",
+       "but WITHOUT ANY WARRANTY; without even the implied warranty of",
+       "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the",
+       "GNU General Public License for more details.\n",
+       "You should have received a copy of the GNU General Public License",
+       "along with this program.  If not, see <http://www.gnu.org/licenses/>."]
